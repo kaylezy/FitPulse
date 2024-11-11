db1ba0590f20c877ffde329df6bdd8b3600bad39
Olakunle Hassan <kunlex_042003@yahoo.com>
2024-11-11 16:57:34 +0100

    fix: some typography

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,3 @@
-// import { defineConfig } from 'vite'
-
-
-// // https://vite.dev/config/
-// export default defineConfig({
-// })
-
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths"
